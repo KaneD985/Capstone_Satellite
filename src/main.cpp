0e3e9910b7f40ec7c545bfc99da5279287f43094
henriquedaponte <henriquedaponte@ucsb.edu>
2024-01-12 01:54:30 -0300

    Worked more on BMI code

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_MLX90614.h>
-#include <bmi323.h>
+#include <math.h>
+#include "bmi323.h"
+#include "common.h"
 
 const int ledPin = 13;
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> CANbus;  // CAN0 is the CAN module to use
@@ -28,6 +30,106 @@ void setup() {
 }
 
 void loop() {
+
+
+
+
+
+  // =================== BMI323 Logic ======================================================
+
+  /* Status of API are returned to this variable. */
+    int8_t rslt;
+
+    /* Variable to define limit to print gyro data. */
+    uint8_t limit = 100;
+
+    float x = 0, y = 0, z = 0;
+    uint8_t indx = 0;
+
+    /* Sensor initialization configuration. */
+    struct bmi3_dev dev = { 0 };
+
+    /* Create an instance of sensor data structure. */
+    struct bmi3_sensor_data sensor_data = { 0 };
+
+    /* Initialize the interrupt status of gyro. */
+    uint16_t int_status = 0;
+
+    /* Structure to define gyro configuration. */
+    struct bmi3_sens_config config = { 0 };
+
+    /* Function to select interface between SPI and I2C, according to that the device structure gets updated.
+     * Interface reference is given as a parameter
+     * For I2C : BMI3_I2C_INTF
+     * For SPI : BMI3_SPI_INTF
+     */
+    rslt = bmi3_interface_init(&dev, BMI3_SPI_INTF);
+    bmi3_error_codes_print_result("bmi3_interface_init", rslt);
+
+    if (rslt == BMI323_OK)
+    {
+        /* Initialize bmi323. */
+        rslt = bmi323_init(&dev);
+        bmi3_error_codes_print_result("bmi323_init", rslt);
+
+        if (rslt == BMI323_OK)
+        {
+            /* Gyro configuration settings. */
+            rslt = set_gyro_config(&dev);
+
+            if (rslt == BMI323_OK)
+            {
+                rslt = bmi323_get_sensor_config(&config, 1, &dev);
+                bmi3_error_codes_print_result("bmi323_get_sensor_config", rslt);
+            }
+
+            if (rslt == BMI323_OK)
+            {
+                /* Select gyro sensor. */
+                sensor_data.type = BMI323_GYRO;
+
+                printf("\nData set, Range, Gyr_Raw_X, Gyr_Raw_Y, Gyr_Raw_Z, Gyr_dps_X, Gyr_dps_Y, Gyr_dps_Z\n\n");
+
+                /* Loop to print gyro data when interrupt occurs. */
+                while (indx <= limit)
+                {
+                    /* To get the data ready interrupt status of gyro. */
+                    rslt = bmi323_get_int1_status(&int_status, &dev);
+                    bmi3_error_codes_print_result("Get interrupt status", rslt);
+
+                    /* To check the data ready interrupt status and print the status for 10 samples. */
+                    if (int_status & BMI3_INT_STATUS_GYR_DRDY)
+                    {
+                        /* Get gyro data for x, y and z axis. */
+                        rslt = bmi323_get_sensor_data(&sensor_data, 1, &dev);
+                        bmi3_error_codes_print_result("Get sensor data", rslt);
+
+                        /* Converting lsb to degree per second for 16 bit gyro at 2000dps range. */
+                        x = lsb_to_dps(sensor_data.sens_data.gyr.x, (float)2000, dev.resolution);
+                        y = lsb_to_dps(sensor_data.sens_data.gyr.y, (float)2000, dev.resolution);
+                        z = lsb_to_dps(sensor_data.sens_data.gyr.z, (float)2000, dev.resolution);
+
+                        /* Print the data in dps. */
+                        printf("%d, %d, %d, %d, %d, %4.2f, %4.2f, %4.2f\n",
+                               indx,
+                               config.cfg.gyr.range,
+                               sensor_data.sens_data.gyr.x,
+                               sensor_data.sens_data.gyr.y,
+                               sensor_data.sens_data.gyr.z,
+                               x,
+                               y,
+                               z);
+
+                        indx++;
+                    }
+                }
+            }
+        }
+    }
+
+    bmi3_coines_deinit();
+
+    // =======================================================================================
   Serial.println("\n Loop Running... \n");
   msg.id = 0x124; // CAN message ID
   msg.len = 8;    // Message length (up to 8 bytes)
