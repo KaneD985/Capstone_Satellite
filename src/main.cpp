8cff3d2cd6d907149a0ef58d3fba9e92dec9a2fa
Kane Deng <ydeng672@umail.ucsb.edu>
2024-04-18 16:56:56 -0700

    Newest Version 4.18

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,8 @@
 const int ledPin = 13;
 const int hallEffectPin = 41;
 const double TIMER_MICROSECONDS = 150000;
-FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> CANbus;  // CAN0 is the CAN module to use
+FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> CANbus;       // For old PCB
+// FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> CANbus;    // For custom PCB
 CAN_message_t msg1;
 CAN_message_t msg2;
 CAN_message_t msg3;
@@ -69,17 +70,17 @@ void setup() {
 
 void loop() {
   // =================== CAN Message Setup ===========================================
-  msg1.id = 0x100; 
-  msg1.len = 8; 
+  // msg1.id = 0x100; 
+  // msg1.len = 8; 
 
-  msg2.id = 0x101; 
-  msg2.len = 8; 
+  // msg2.id = 0x101; 
+  // msg2.len = 8; 
 
   msg3.id = 0x102; 
   msg3.len = 8;    
 
-  msg4.id = 0x103; 
-  msg4.len = 8;    
+  // msg4.id = 0x103; 
+  // msg4.len = 8;    
 
   Serial.println("\n Loop Running... \n");
 
@@ -99,9 +100,6 @@ void loop() {
   // float average = sum / (32*24);
   // Serial.print("Average: "); 
   // Serial.println(average);
-  // float average = sum / (32*24);
-  // Serial.print("Average: "); 
-  // Serial.println(average);
 
   // delay(500);
 
@@ -112,22 +110,33 @@ void loop() {
   int sendAmbVal = tempA * 100;
 
   //===================== Data Push ==================================================
-  msg1.buf[0] = sendObjVal >> 8;
-  msg1.buf[1] = sendObjVal & 0xFF;
-  msg1.buf[2] = sendAmbVal >> 8;
-  msg1.buf[3] = sendAmbVal & 0xFF;
+  // msg1.buf[0] = sendObjVal >> 8;
+  // msg1.buf[1] = sendObjVal & 0xFF;
+  // msg1.buf[2] = sendAmbVal >> 8;
+  // msg1.buf[3] = sendAmbVal & 0xFF;
 
-  msg2.buf[0] = 1 >> 8;
-  msg2.buf[1] = 1 & 0xFF;
+  // msg2.buf[0] = sendObjVal >> 8;
+  // msg2.buf[1] = sendObjVal & 0xFF;
+  // msg2.buf[2] = sendAmbVal >> 8;
+  // msg2.buf[3] = sendAmbVal & 0xFF;
+
   
-  msg3.buf[0] = 2 >> 8;
-  msg3.buf[1] = 2 & 0xFF;
+  msg3.buf[0] = sendObjVal >> 8;
+  msg3.buf[1] = sendObjVal & 0xFF;
+  msg3.buf[2] = sendAmbVal >> 8;
+  msg3.buf[3] = sendAmbVal & 0xFF;
 
-  msg4.buf[0] = 3 >> 8;
-  msg4.buf[1] = 3 & 0xFF;
+  // msg4.buf[0] = sendObjVal >> 8;
+  // msg4.buf[1] = sendObjVal & 0xFF;
+  // msg4.buf[2] = sendAmbVal >> 8;
+  // msg4.buf[3] = sendAmbVal & 0xFF;
 
-  bool writeResult = CANbus.write(msg1) & CANbus.write(msg2) & CANbus.write(msg3) & CANbus.write(msg4);  // Send the message
+  // bool writeResult = CANbus.write(msg1) & CANbus.write(msg2) & CANbus.write(msg3) & CANbus.write(msg4);  // Send the message
   // bool writeResult = CANbus.write(msg1);
+  // bool writeResult = CANbus.write(msg2);
+  bool writeResult = CANbus.write(msg3);
+  // bool writeResult = CANbus.write(msg4);
+
   Serial.print("Writing Message: ");
   Serial.println(writeResult);  // Print the result of the write operation
 
