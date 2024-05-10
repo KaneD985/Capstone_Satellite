fea0e534350e15f209b003bd30c15150b1667c1d
Kane Deng <ydeng672@umail.ucsb.edu>
2024-05-09 19:55:00 -0700

    Raw code 5.9
    Raw code 5.9

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <TeensyThreads.h>
 #include <queue>
+#include <SD.h>
 
 #define INC_ADDRESS 0x68
 #define ACC_CONF  0x20  //Page 91
@@ -16,9 +17,10 @@
 //  =================== CAN Variables ===============================================
 const int ledPin = 13;
 const int hallEffectPin = 41;
+const int chipSelect = BUILTIN_SDCARD;
 const double TIMER_MICROSECONDS = 150000;
-FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> CANbus;       // For old PCB
-// FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> CANbus;    // For custom PCB
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> CANbus;    // For custom PCB IT'S CAN 2 !!!!!! DONT FUCKING CHANGE IT 
+// FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> CANbus;    // For old PCB IT'S CAN 3 !!!!!!!!!!!!!!
 CAN_message_t msg1;
 CAN_message_t msg2;
 CAN_message_t msg3;
@@ -28,6 +30,9 @@ CAN_message_t msg4;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 Adafruit_MLX90640 mlx2;
 
+// =================== BMI Variables ================================================
+uint16_t gyr_x, gyr_y, gyr_z;
+int16_t signed_gyr_x, signed_gyr_y, signed_gyr_z;
 
 // =================== Hall Effect Variables ========================================
 IntervalTimer halleffectTimer;
@@ -74,7 +79,15 @@ void setup() {
   pinMode(hallEffectPin, INPUT);
   threads.addThread(thread_func, 1);
   halleffectTimer.begin(calculateRPM, TIMER_MICROSECONDS);
+
   Serial.begin(115200);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");\
+    return;
+  }
+  Serial.println("card initialized.");
+  
   while (!Serial && millis() < 4000);
   Wire.begin();
   Wire.setClock(400000); 
@@ -92,11 +105,11 @@ void loop() {
   // msg1.id = 0x100; 
   // msg1.len = 8; 
 
-  // msg2.id = 0x101; 
-  // msg2.len = 8; 
+  msg2.id = 0x101; 
+  msg2.len = 8; 
 
-  msg3.id = 0x102; 
-  msg3.len = 8;    
+  // msg3.id = 0x102; 
+  // msg3.len = 8;    
 
   // msg4.id = 0x103; 
   // msg4.len = 8;    
@@ -115,7 +128,6 @@ void loop() {
     }
     printf("\n");
   }
-  
   float average = sum / (32*24);
   Serial.print("Average: "); 
   Serial.println(average);
@@ -128,22 +140,33 @@ void loop() {
   int sendObjVal = tempO * 100;
   int sendAmbVal = tempA * 100;
 
+  //===================== SD Card Data Logging ======================================
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  // if the file is available, write the contents of datastring to it
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }  
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }   
+
   //===================== Data Push ==================================================
   // msg1.buf[0] = sendObjVal >> 8;
   // msg1.buf[1] = sendObjVal & 0xFF;
   // msg1.buf[2] = sendAmbVal >> 8;
   // msg1.buf[3] = sendAmbVal & 0xFF;
 
-  // msg2.buf[0] = sendObjVal >> 8;
-  // msg2.buf[1] = sendObjVal & 0xFF;
-  // msg2.buf[2] = sendAmbVal >> 8;
-  // msg2.buf[3] = sendAmbVal & 0xFF;
+  msg2.buf[0] = sendObjVal >> 8;
+  msg2.buf[1] = sendObjVal & 0xFF;
+  msg2.buf[2] = sendAmbVal >> 8;
+  msg2.buf[3] = sendAmbVal & 0xFF;
 
-  
-  msg3.buf[0] = sendObjVal >> 8;
-  msg3.buf[1] = sendObjVal & 0xFF;
-  msg3.buf[2] = sendAmbVal >> 8;
-  msg3.buf[3] = sendAmbVal & 0xFF;
+  // msg3.buf[0] = sendObjVal >> 8;
+  // msg3.buf[1] = sendObjVal & 0xFF;
+  // msg3.buf[2] = sendAmbVal >> 8;
+  // msg3.buf[3] = sendAmbVal & 0xFF;
 
   // msg4.buf[0] = sendObjVal >> 8;
   // msg4.buf[1] = sendObjVal & 0xFF;
@@ -152,8 +175,8 @@ void loop() {
 
   // bool writeResult = CANbus.write(msg1) & CANbus.write(msg2) & CANbus.write(msg3) & CANbus.write(msg4);  // Send the message
   // bool writeResult = CANbus.write(msg1);
-  // bool writeResult = CANbus.write(msg2);
-  bool writeResult = CANbus.write(msg3);
+  bool writeResult = CANbus.write(msg2);
+  // bool writeResult = CANbus.write(msg3);
   // bool writeResult = CANbus.write(msg4);
 
   Serial.print("Writing Message: ");
